b71a8fbd21b68ef2e203bc6d7a5777cf96b94f6d
gilangr1440 <gilangr1440@gmail.com>
2024-11-10 17:02:19 +0700

    newsletter sign up

---
+++ script.js
@@ -0,0 +1,34 @@
+const input = document.querySelector(".main-content__form-input");
+const button = document.querySelector(".main-content__form-button");
+const errorText = document.querySelector(".main-content__error-text");
+const success = document.querySelector(".success-card");
+const closeButton = document.querySelector(".success-card__button");
+const emailText = document.querySelector(".success-card__desc_bold");
+const check = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+button.addEventListener("click", () => {
+  if (input.value == "") {
+    errorText.classList.add("active");
+    input.classList.add("error");
+    errorText.innerText = "Email is required";
+  } else if (!check.test(input.value)) {
+    errorText.classList.add("active");
+    input.classList.add("error");
+    errorText.innerText = "Valid email required";
+  } else if (check.test(input.value)) {
+    errorText.classList.remove("active");
+    input.classList.remove("error");
+    success.classList.add("active");
+    emailText.innerText = input.value;
+  }
+});
+
+closeButton.onclick = () => {
+  success.style.animation = "disappear 0.5s";
+
+  setTimeout(() => {
+    success.classList.remove("active");
+    success.style.animation = "appear 0.5s";
+  }, 500);
+  input.value = "";
+};
